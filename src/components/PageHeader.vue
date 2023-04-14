4aee8e10d578ec550be1eef4e2a1aeacb1034605
dannyganza <kingdanny@Dannys-Air.hitronhub.home>
2023-04-13 19:26:33 -0600

    added vue files

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+<h2>This is so cool</h2>
+<p>Hello this is my first time tryig this</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
